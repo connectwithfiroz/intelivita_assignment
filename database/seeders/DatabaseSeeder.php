d3a5fc1d9dd30ec26117f30e690a8b75f87d91f8
Firoz <contacttofiroz@gmail.com>
2025-04-25 00:10:32 +0530

    readme added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            // UserSeeder::class,
+            UserSeeder::class,
             UserActivitySeeder::class,
         ]);
     }
